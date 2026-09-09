cd9b8632b4dc9b4a37d990d20d1a947788cc4a7f
Denis Temnikov <vagim.temnikov.2007@gmail.com>
2026-09-09 22:17:27 +0500

    Fix router basename for GitHub Pages

--- FBAsite/src/main.jsx
+++ FBAsite/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/FBA-site/">
       <App />
     </BrowserRouter>
   </StrictMode>,
