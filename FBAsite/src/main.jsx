c36848e46d15fb4fd168030289d079f0fc02bdb3
Denis Temnikov <vagim.temnikov.2007@gmail.com>
2026-09-13 16:39:10 +0500

    redirection

--- FBAsite/src/main.jsx
+++ FBAsite/src/main.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const redirectPath = sessionStorage.getItem('redirectPath');
+if (redirectPath) {
+    sessionStorage.removeItem('redirectPath');
+    window.history.replaceState(null, '', redirectPath);
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/">
