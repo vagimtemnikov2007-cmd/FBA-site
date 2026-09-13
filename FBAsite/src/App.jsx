e37e74a09f3b23517604f4222996dd709cb9b44d
Denis Temnikov <vagim.temnikov.2007@gmail.com>
2026-09-13 15:47:40 +0500

    NEW HOME

--- FBAsite/src/App.jsx
+++ FBAsite/src/App.jsx
@@ -1,11 +1,48 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import { Routes , Route} from 'react-router-dom'
 import AnimationsLayout from './layout/AnimationsLayout/Animationslayout'
 import HomeLayout from './layout/HomeLayout/Homelayout'
 import './App.css'
 
+
+function getVisitorID() {
+    let id = localStorage.getItem('visitorID');
+    if (!id) {
+      id = crypto.randomUUID();
+      localStorage.setItem('visitorID', id);
+    }
+    return id;
+    
+}
+
 function App() {
-  
+
+  useEffect(() => {
+    const today = new Date().toISOString().slice(0, 10)
+    const lastVisitDate = localStorage.getItem('lastVisitDate')
+
+    if (!lastVisitDate || lastVisitDate !== today) {
+      const visitorId = getVisitorID()
+
+      fetch('https://fba-server.onrender.com/visitors', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ visitorId }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Visitor ID sent to server:', data)
+
+          localStorage.setItem('lastVisitDate', today)
+        })
+        .catch(error => {
+          console.error('Error sending visitor ID to server:', error)
+        })
+    }
+  }, [])
+
   return (
     <>
     <Routes>
@@ -16,4 +53,5 @@ function App() {
   )
 }
 
+getVisitorID()
 export default App
--- FBAsite/src/pages/Home/Home.jsx
+++ FBAsite/src/pages/Home/Home.jsx
@@ -1,9 +1,22 @@
+import React from 'react';
+import './Home.css';
+
+import FBALOGO from '../../assets/FBA-LOGO.png';
+
 function Home() {
     return (
         <div className="home">
-            <h1>Welcome to Fresh Block Animations</h1>
+            <img className="home-image" src={FBALOGO} alt="Fresh Block Animations" />
+            <h1>Make your blocks come alive!</h1>
+
+            <div className="button-container">
+            <button className="home-button INSTALL" onClick={() => window.location.href = 'https://modrinth.com/mod/fresh-block-animations'}>Install Mod</button>
+            <button className="home-button ANIMATIONS" onClick={() => window.location.href = '/animations'}>View Animations</button>
+            <button className="home-button YOUTUBE" onClick={() => window.location.href = 'https://www.youtube.com/@%D0%B1%D0%B0%D1%81%D0%B8%D0%BB11'}>Youtube</button>
+            <button className="home-button DISCORD" onClick={() => window.location.href = 'https://discord.gg/FGdjkw6RDA'}>Discord</button>
+            </div>
             <p>Fresh Block Animations is a Minecraft mod that adds new animations to blocks in the game. It is designed to enhance the visual experience of Minecraft by making blocks feel more alive and interactive.</p>
-            <p>To get started, you can install the mod from <a href="https://modrinth.com/mod/fresh-block-animations">Modrinth</a>. Once installed, you can explore the various animations available in the mod and see how they enhance your Minecraft experience.</p>
+            <p>To get started, you can install the mod from <a className="home-link" href="https://modrinth.com/mod/fresh-block-animations">Modrinth</a>. Once installed, you can explore the various animations available in the mod and see how they enhance your Minecraft experience.</p>
             <p>Feel free to check out the animations page to see all the animations included in the mod, and don't forget to check for updates regularly!</p>
         </div>
     );
